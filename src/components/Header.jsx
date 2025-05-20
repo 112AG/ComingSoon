50e6fcbb44060eb015e792643176d6ed8c1cf579
Gaurav Twiari <gauravtiwari812798@gmail.com>
2025-05-20 18:57:01 +0530

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import viteLogo from '../../public/SDFinance.svg';
+const viteLogo = '/SDFinance.svg';
 import { NavigationContext } from '../utils/NavigationProvider ';
 
 function Header() {
